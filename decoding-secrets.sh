bf9e49c6003fbe8c3e0ef155267f432d4cc57845
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2022-08-11 16:21:55 +0530

    Update decoding-secrets.sh

--- decoding-secrets.sh
+++ decoding-secrets.sh
@@ -14,9 +14,9 @@ then
     echo " choose values to Decode, can take two values  example: USERNAME PASSWORD "
     read value1 value2
     echo " Decoding $value1 "
-    echo -n  "$value1="; kubectl get secret $secret -n $namespace -o json | jq .data.$value1 | sed 's/"//g' | base64 --decode
+    echo -n  "$value1="; kubectl get secret $secret -n $namespace -o jsonpath={.data.$value1} | base64 --decode
     echo " Decoding $value2 "
-    echo -n  "$value2="; kubectl get secret $secret -n $namespace -o json | jq .data.$value2 | sed 's/"//g' | base64 --decode
+    echo -n  "$value2="; kubectl get secret $secret -n $namespace -o jsonpath={.data.$value2} | base64 --decode
 elif  [ "$1" = "docker" ]
 then
      kubectl get ns
@@ -27,5 +27,5 @@ then
      read secret
      kubectl describe secret $secret -n $namespace
      echo " Decoding Docker Registry Credentials "
-     kubectl get secret $secret -n $namespace -o json | jq .data | sed 's/"//g' | awk '{print $2}' | base64 --decode
+     kubectl get secret $secret -n $namespace -o jsonpath={.data.*} | base64 --decode
 fi
